a028b3c25f800cde2be50fdc1afa83e36fe5e7a8
Alper Karaca <alpeerkaraca@gmail.com>
2022-12-03 19:50:07 +0300

    DM for suggestions

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using System.IO;
+using System.Net.Http;
+using System.Globalization;
+using System.Diagnostics;
+using System.Text;
+namespace Yemek{
+    class Program
+    {
+        public static void Main(){
+            DateTime date= DateTime.Now;
+            String[] foodListHolder;
+            String[] foodList = new string[5];
+            Console.OutputEncoding = Encoding.UTF8;
+            int i = 0;
+
+            var url = "https://sks.omu.edu.tr/gunun-yemegi/";
+            
+            var HttpClient = new HttpClient();
+
+            var html = HttpClient.GetStringAsync(url);
+
+            var doc = new HtmlDocument();
+
+            doc.LoadHtml(html.Result);
+
+            var htmlNodes = doc.DocumentNode.SelectNodes("//div/table/tbody/tr");
+            foodListHolder = new string[htmlNodes.Count];
+            foreach(var htmlNode in htmlNodes) foodListHolder[i++] = htmlNode.InnerText;
+            int index = Int32.Parse(date.Day.ToString()) + 2;
+            foodList = foodListHolder[index].Split("\n",System.StringSplitOptions.RemoveEmptyEntries);
+
+            Console.Write($"️🗓️Bugünün Tarihi: {date.ToLongDateString()}\nGünün Çorbası: {foodList[2]}\n🍱Ana Yemek: {foodList[3]}\n🍚Ara Yemek: {foodList[4]}");
+            Console.Write($"\n🍑Yan Ürün: {foodList[5]}\n\U0001f944İblis Aletta'dan Bol Afiyetlerle 🧑🏼‍🍳");
+        /*
+            Instagram: https://instagram.com/alpeerkaraca
+            Github: https://github.com/alpeerkaraca
+            Twitter: https://twitter.com/alpeerkaraca
+            Linkedin: https://linkedin.com/in/alpeerkaraca
+            Discord: Mashiro Shiina#7086
+            Website: https://alpeerkaraca.github.io
+        */
+        }
+
+    }
+
+}       //Index = current day + 2 (cause of empty lines on html code)
---
+++ README.md
@@ -0,0 +1,12 @@
+# Ondokuz Mayıs Üniversitesi Günün Yemeği Konsol Uygulaması
+Üniversitemizin web sayfasına girmek bugün hangi yemek var acaba diye listede gezinmek güncellenmemiş görsele aldanıp yanlış yemeğe gitmek ve sade tasarımlı web sayfası gibi şeylerden sıkıldınız mı?
+İşte burada bu uygulama devreye giriyor. 
+Web Scraping (Web Kazıma) çalışırken aklıma gelen böyle bir fikir sonrasında bu ürün sizlerle buluşuyor.
+Artık yarım saat boyunca telefonunuzun tarayıcısına girip arama yapmaktansa komut satırınıza yazacağınız tek kelime ile günün yemeğine ve Garson Aletta'nın sözüyle yemek sırasına girebilirsiniz.
+İndirmek için [Buraya Tıklayın](releases) ya da Releases sekmesinden indirin.
+## Windows için Path'e Ekleme
+[Windows için Path'e Ekleme](https://www.routech.ro/tr/windows-path-nedir-ve-nasil-ekler-ve-duzenlersiniz/)
+## Linux İçin Path'e Ekleme
+[Linux İçin Path'e Dosya Ekleme](https://gdemir.github.io/categories/linux/path-ekle/)
+## İletişim
+[Web Sayfam](https://alpeerkaraca.github.io)
